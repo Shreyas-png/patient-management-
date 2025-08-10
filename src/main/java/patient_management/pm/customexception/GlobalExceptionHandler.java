105b76b53d4e8873349a4b9038162d5837411611
Shreyas k <shreyaskulkarni991@gmail.com>
2025-08-11 00:07:29 +0530

    exception changes

--- src/main/java/patient_management/pm/customexception/GlobalExceptionHandler.java
+++ src/main/java/patient_management/pm/customexception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package patient_management.pm.customexception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,12 +11,14 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceAlreadyExist.class)
     public ResponseEntity<ApiError> resourceAlreadyExistHandler(ResourceAlreadyExist ex) {
         ApiError error = new ApiError(400, ex.getMessage(), "Duplicate Entry", LocalDateTime.now());
+        log.warn(ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
@@ -23,12 +26,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> validationExceptionHandler(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        log.warn(ex.getMessage());
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceDoesNotExist.class)
     public ResponseEntity<ApiError> resourceDoesNotExistExceptionHandler(ResourceDoesNotExist ex){
         ApiError error = new ApiError(404, ex.getMessage(), "Not Found", LocalDateTime.now());
+        log.warn(ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
