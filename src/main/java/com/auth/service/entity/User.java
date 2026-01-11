f25893c157229f3a6a1f13a1d50c9836c410abcf
Shreyas K <shreyaskulkarni991@gmail.com>
2026-01-11 22:21:28 +0530

    initial commit

---
+++ src/main/java/com/auth/service/entity/User.java
@@ -0,0 +1,32 @@
+package com.auth.service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
